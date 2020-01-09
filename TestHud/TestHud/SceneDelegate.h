a0a30874aa724e38dade44981e1a76cb8a6f4631
iucrzy <guohongyang@yqzbw.com>
2020-01-09 15:20:07 +0800

    新建工程

---
+++ TestHud/TestHud/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  TestHud
+//
+//  Created by iucrzy on 2020/1/9.
+//  Copyright © 2020 lyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ TestHud/TestHud/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestHud
+//
+//  Created by iucrzy on 2020/1/9.
+//  Copyright © 2020 lyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
