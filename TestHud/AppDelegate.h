3191333830283d9033b2ab7c5d18109676d9b0e7
iucrzy <guohongyang@yqzbw.com>
2020-01-16 14:22:07 +0800

    no message

---
+++ TestHud/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestHud
+//
+//  Created by iucrzy on 2020/1/9.
+//  Copyright © 2020 lyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
