480c74ca85598e9ca57aa2960a00ae7c8ec9dbc7
iucrzy <guohongyang@yqzbw.com>
2020-01-16 10:47:00 +0800

    修正podspec文件

---
+++ TestHud/TestHud/Hud/MMRHud.h
@@ -0,0 +1,17 @@
+//
+//  MMRHud.h
+//  TestHud
+//
+//  Created by iucrzy on 2020/1/9.
+//  Copyright © 2020 lyh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MMRHud : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestHud/TestHud/Hud/MMRHud.m
@@ -0,0 +1,13 @@
+//
+//  MMRHud.m
+//  TestHud
+//
+//  Created by iucrzy on 2020/1/9.
+//  Copyright © 2020 lyh. All rights reserved.
+//
+
+#import "MMRHud.h"
+
+@implementation MMRHud
+
+@end
